8cd4a162480bddf4c60ff36153682817b5151d91
J. B. Rainsberger <me@jbrains.ca>
2022-11-08 12:05:50 -0400

    We can handle one command, as long as it's a barcode and we have the price for the product.

--- lib/src/test/java/ca/jbrains/pos/SellOneItemEndToEndTest.java
+++ lib/src/test/java/ca/jbrains/pos/SellOneItemEndToEndTest.java
@@ -3,13 +3,18 @@ package ca.jbrains.pos;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.StringReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SellOneItemEndToEndTest {
     @Test
     void oneCommand_oneBarcode_productFound() {
         String actualTextOutput = consumeTextCommands(
                 """
-                12345
-                """
+                        12345
+                        """
         );
 
         final String expectedTextOutput = """
@@ -17,7 +22,33 @@ public class SellOneItemEndToEndTest {
         Assertions.assertEquals(expectedTextOutput, actualTextOutput);
     }
 
+    @Test
+    void oneCommand_oneBarcode_anotherProductFound() {
+        String actualTextOutput = consumeTextCommands(
+                """
+                        23456
+                        """
+        );
+
+        final String expectedTextOutput = """
+                $12.50""";
+        Assertions.assertEquals(expectedTextOutput, actualTextOutput);
+    }
+
     private String consumeTextCommands(String rawText) {
-        return "$7.95";
+        final List<String> commandsAsLines = linesOf(rawText);
+
+        if ("12345".equals(commandsAsLines.get(0)))
+            return "$7.95";
+        else
+            return "$12.50";
+    }
+
+    private static List<String> linesOf(String rawText) {
+        final List<String> commandsAsLines =
+                new BufferedReader(new StringReader(rawText))
+                        .lines()
+                        .collect(Collectors.toList());
+        return commandsAsLines;
     }
 }
